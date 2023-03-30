67ecade5fafa0c51fd115f3efcdfeb604c8cc4df
Source-Kod <Source-Kod@protonmail.com>
2023-03-29 23:33:59 -0500

    added random colours option

--- script.js
+++ script.js
@@ -26,6 +26,16 @@ function startBrush(colour) {
   });
 }
 
+function startRandomColourBrush() {
+  let [...cells] = document.getElementsByClassName('cell');
+
+  cells.forEach(element => {
+   element.addEventListener('mouseover', () => {
+      element.className = "cell " + randomColour();
+    },true);
+  });
+}
+
 function colourChanger(color) {
   const chosen_color_preview = document.querySelector('#chosen_color_preview');
   chosen_color_preview.style.backgroundColor = color;
@@ -33,37 +43,63 @@ function colourChanger(color) {
 }
 
 function createEventsForOptions(){
-  const erase_btn = document.querySelector('#erase_btn')
+  const random_btn = document.querySelector('#random_btn');
+  random_btn.addEventListener('click', () => {
+    startRandomColourBrush();
+  });
+
+  const erase_btn = document.querySelector('#erase_btn');
   erase_btn.addEventListener('click', () => {
     colourChanger('lightgrey')
-  })
+  });
 
-  const red_btn = document.querySelector('#red_btn')
+  const red_btn = document.querySelector('#red_btn');
   red_btn.addEventListener('click', () => {
     colourChanger('red')
-  })
+  });
 
-  const green_btn = document.querySelector('#green_btn')
+  const green_btn = document.querySelector('#green_btn');
   green_btn.addEventListener('click', () => {
-    colourChanger('green')
-  })
+    colourChanger('green');
+  });
 
-  const blue_btn = document.querySelector('#blue_btn')
+  const blue_btn = document.querySelector('#blue_btn');
   blue_btn.addEventListener('click', () => {
     colourChanger('blue')
-  })
+  });
 
-  const orange_btn = document.querySelector('#orange_btn')
+  const orange_btn = document.querySelector('#orange_btn');
   orange_btn.addEventListener('click', () => {
-    colourChanger('orange')
+    colourChanger('orange');
   })
 
-  const purple_btn = document.querySelector('#purple_btn')
+  const purple_btn = document.querySelector('#purple_btn');
   purple_btn.addEventListener('click', () => {
-    colourChanger('purple')
+    colourChanger('purple');
   })
+}
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
+function randomColour() {
+  let x = random(1, 5);
+
+  switch (x) {
+    case 1:
+      return "purple";
+    case 2:
+      return "green";
+    case 3:
+      return "blue";
+    case 4:
+      return "red";
+    case 5:
+      return "orange";
+    default:
+      return "error";
+  }
 }
 // Call the createGrid function to generate the grid
 createGrid();
